b23dd71315917f3f4110743c6903f7b8e616f56f
Murat İleri <mi.muratt@gmail.com>
2023-06-12 23:30:32 +0300

    Revert "deneme"
    This reverts commit 376c8fb18dede9544a26bf868214a3959a3a7f4c.

--- src/db_connect.php
+++ src/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-	$con = mysqli_connect('localhost', 'root', '', 'library_system');
+	$con = mysqli_connect('localhost', 'root', '', 'library-system');
 	if(!$con)
 		die("ERROR: Couldn't connect to database");
 ?>
--- src/member/header_member.php
+++ src/member/header_member.php
@@ -13,37 +13,19 @@
 			</div>
 			
 			<div class="dropdown">
-				<?php
-				// Retrieve the image data from the database based on the logged-in user's information
-				$query = $con->prepare("SELECT profile_image FROM useraccount WHERE username = ?");
-				$username = $_SESSION['username'];
-				$query->bind_param("s", $username);
-				$query->execute();
-				$result = $query->get_result();
-
-				if ($result->num_rows > 0) {
-					// Fetch the row containing the image data
-					$row = $result->fetch_assoc();
-					
-					// Assign the image data to the $imgContent variable
-					$imgContent = $row['profile_image'];
-				}
-				?>
-
 				<button class="dropbtn">
-				<?php
-					// Check if the profile image exists
-					if ($imgContent !== null) {
-					echo '<img src="data:image/jpeg;base64,' . base64_encode($imgContent) . '" alt="Profile Image" width="24" height="24">';
-					}
-				?>
-				<p id="librarian-name"><?php echo $_SESSION['username'] ?></p>
+					<p id="librarian-name"><?php echo $_SESSION['username'] ?></p>
 				</button>
-
-
 				<div class="dropdown-content">
 					
 				<a href="my_books.php">My books</a>
+				<a href="../insert_book.php">Insert Book</a>
+				<a href=".././librarian/book_details.php">Book Details</a>
+                <a href=".././librarian/copies_per_branch.php">Copies Per Branch</a>
+                <a href=".././librarian/top_rated_books.php">Top Rated Books</a>
+                <a href=".././librarian/borrower_total_fine.php">Borrower Total Fine</a>
+                <a href=".././librarian/overdue_loans.php">Overdue Loans</a>
+                <a href=".././librarian/stats.php">Stats</a>
                 <a href="../logout.php">Logout</a>
 
 
